80c3ce9c8d0b1f5cc6012ad052fd73904f236d04
Kosuke Miyoshi <miyoshi@narr.jp>
2015-07-04 21:07:13 +0900

    set default filename at save panel

--- heartrate-monitor/ViewController.swift
+++ heartrate-monitor/ViewController.swift
@@ -11,20 +11,20 @@ import Cocoa
 class ViewController: NSViewController, HeartRateDelegate {
 	@IBOutlet weak var startButton: NSButtonCell!
 
-    @IBOutlet weak var stateLabel: NSTextField!
-    @IBOutlet weak var heartRateRRCountLabel: NSTextField!
-    @IBOutlet weak var heartRateValueLabel: NSTextField!    
-    @IBOutlet weak var durationLabel: NSTextField!
-    
+	@IBOutlet weak var stateLabel: NSTextField!
+	@IBOutlet weak var heartRateRRCountLabel: NSTextField!
+	@IBOutlet weak var heartRateValueLabel: NSTextField!
+	@IBOutlet weak var durationLabel: NSTextField!
+
 	var heartRateCenter: HeartRateCenter!
 	var heartRateRRIntervalDatas: [Double]!
-    var heartRateRRCount = 0;
-    var duration = 0.0;
-    
-    override func viewDidLoad() {
+	var heartRateRRCount = 0;
+	var duration = 0.0;
+
+	override func viewDidLoad() {
 		super.viewDidLoad()
-        
-        stateLabel.stringValue = "init"
+
+		stateLabel.stringValue = "init"
 	}
 
 	override func viewWillDisappear() {
@@ -45,8 +45,8 @@ class ViewController: NSViewController, HeartRateDelegate {
 		if (heartRateCenter == nil) {
 			heartRateCenter = HeartRateCenter(delegate: self)
 			heartRateCenter.setup()
-            
-            stateLabel.stringValue = "connecting"
+
+			stateLabel.stringValue = "connecting"
 
 			heartRateRRIntervalDatas = [Double]()
 
@@ -56,7 +56,7 @@ class ViewController: NSViewController, HeartRateDelegate {
 			heartRateCenter = nil
 			startButton.title = "Start"
 
-            stateLabel.stringValue = "init"
+			stateLabel.stringValue = "init"
 
 			saveData()
 		}
@@ -65,49 +65,60 @@ class ViewController: NSViewController, HeartRateDelegate {
 	func saveData() {
 		if heartRateRRIntervalDatas.count > 0 {
 			var panel = NSSavePanel()
-            panel.nameFieldLabel = "File Name"
-            panel.beginWithCompletionHandler( { (result:Int) -> Void in
-                if result == NSFileHandlingPanelOKButton {
-                    var saveURL = panel.URL
-                    if( saveURL != nil ) {
-                        println("save url=\(saveURL)")
-                        self.saveRRIntervalData(saveURL!)
-                    }
-                }
-            } )
-        }
+			panel.nameFieldLabel = "File Name"
+
+            // set default filename
+			let dateFormatter = NSDateFormatter()
+			dateFormatter.locale = NSLocale(localeIdentifier: "en_US")
+			dateFormatter.dateFormat = "yyyyMMdd_HHmmss"
+            let now = NSDate()
+			let dateStr = dateFormatter.stringFromDate(now)
+            panel.nameFieldStringValue = String(format:"rr_\(dateStr).txt")
+
+            // show save dialog
+			panel.beginWithCompletionHandler({
+				(result: Int) -> Void in
+				if result == NSFileHandlingPanelOKButton {
+					var saveURL = panel.URL
+					if (saveURL != nil) {
+						println("save url=\(saveURL)")
+						self.saveRRIntervalData(saveURL!)
+					}
+				}
+			})
+		}
 	}
-    
-    func saveRRIntervalData(url:NSURL) {
-        let path = url.path!
-        var content = ""
-        for rr in heartRateRRIntervalDatas {
-            content += (String("\(rr)") + "\n")
-        }
-        content.writeToFile(path, atomically: false, encoding: NSUTF8StringEncoding, error: nil);
-    }
+
+	func saveRRIntervalData(url: NSURL) {
+		let path = url.path!
+		var content = ""
+		for rr in heartRateRRIntervalDatas {
+			content += (String("\(rr)") + "\n")
+		}
+		content.writeToFile(path, atomically: false, encoding: NSUTF8StringEncoding, error: nil);
+	}
 
 
 	func heartRateDeviceDidConnect() {
 		println("<connect>")
-        stateLabel.stringValue = "connected"
+		stateLabel.stringValue = "connected"
 	}
 
 	func heartRateDeviceDidDisconnect() {
 		println("<disconnect>")
-        stateLabel.stringValue = "disconnected"
+		stateLabel.stringValue = "disconnected"
 	}
 
 	func heartRateRRDidArrive(rr: Double) {
 		println("<rr=\(rr)>")
 		heartRateRRIntervalDatas.append(rr);
-        
-        duration += (rr / 1000.0);
-        
-        heartRateRRCount++;
-        heartRateRRCountLabel.stringValue = String("\(heartRateRRCount)")
-        var heartRateValue = 60.0 * 1000.0 / rr;
-        heartRateValueLabel.stringValue = String(format:"%.2f", heartRateValue)
-        durationLabel.stringValue = String(format:"%.1f sec", duration)
+
+		duration += (rr / 1000.0);
+
+		heartRateRRCount++;
+		heartRateRRCountLabel.stringValue = String("\(heartRateRRCount)")
+		var heartRateValue = 60.0 * 1000.0 / rr;
+		heartRateValueLabel.stringValue = String(format: "%.2f", heartRateValue)
+		durationLabel.stringValue = String(format: "%.1f sec", duration)
 	}
 }
